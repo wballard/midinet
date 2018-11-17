541444316bc016dd43133c89901a27ec2429f1d0
Will Ballard <wballard@mailframe.net>
2018-11-17 08:40:57 -0600

    coding and decoding, tested with timidity -- files actually play

--- midinet/codec.py
+++ midinet/codec.py
@@ -11,18 +11,20 @@ import numpy as np
 
 class Encoder:
     '''Encode a MIDI file as a vectorized bitmap pattern.
-    >>> enc = Encoder("var/data/Dancing Queen - Chorus.midi")
+    >>> enc = Encoder(mido.MidiFile("var/data/Dancing Queen - Chorus.midi"))
     >>> enc.numpy().shape
-    (398, 32)
+    (3, 252, 32)
+    >>> enc.numpy().dtype
+    dtype('uint8')
     '''
 
     def __init__(self, midifile):
         '''
         
         Arguments:
-            midifile {string} -- file name of a source midi
+            midifile {mido.MidiFile} -- file object containing midi
         '''
-        self.midifile = mido.MidiFile(midifile)
+        self.midifile = midifile 
 
     def numpy(self):
         '''Numpy encoding of the MIDI content.
@@ -32,10 +34,11 @@ class Encoder:
         as raw as possible.
 
         Returns:
-            np.array -- a two dimensional [message, bits] array with 32 bits per message
+            np.array -- a three dimensional [track, message, bits] array with 32 bits per message
         '''
-        buffer = []
-        for track in self.midifile.tracks:
+        track_buffer = []
+        for i, track in enumerate(self.midifile.tracks):
+            buffer = []
             for msg in track:
                 if msg.type in ['note_on', 'note_off']:
                     # max timing length of 255 for a single byte of timing
@@ -45,23 +48,67 @@ class Encoder:
                     # packing into a single bit pattern
                     one_hot = np.concatenate([tone, timing])
                     buffer.append(one_hot)
-        return np.vstack(buffer)
+            track_buffer.append(np.vstack(buffer))
+        # this needs to be rectangular, not ragged, so we will be padding
+        max_length = max([len(buffer) for buffer in track_buffer])
+        padded_buffers = []
+        for buffer in track_buffer:
+            padded_buffer = np.pad(buffer, ((0, max_length-len(buffer)), (0, 0)), 'constant', constant_values=0)
+            padded_buffers.append(padded_buffer)
+        return np.stack(padded_buffers, axis=0)
 
 
 class Decoder:
+    '''Decode a tensor representation MIDI stream.
+
+    Each input bit may be a floating point and need to be rounded to a hard 1 or 0.
+    >>> enc = Encoder(mido.MidiFile("var/data/Dancing Queen - Chorus.midi"))
+    >>> dec = Decoder(enc.numpy())
+    >>> midi = dec.midi()
+    >>> midi.save("var/scratch/test.midi")
+    '''
+
     def __init__(self, miditensor):
         '''
         
         Arguments:
             miditensor {numpy.ndarray} -- MIDI encoded with {Encoder}
         '''
-        pass
+        self.miditensor = miditensor
 
-    def midi(self):
-        '''{mido.MidiFile} representation.
+    def midi(self, ticks_per_beat=128):
+        '''Transforms the encoded midi tensor into a file object which can then be saved.
+        
+        Arguments:
+            ticks_per_beat {int} -- controls the tempo of the playback, higher is faster
+        
+        Returns:
+            {mido.MidiFile} representation.
         '''
-        pass
+        # first -- round and decode the message bits
+        as_bytes = np.packbits(np.around(self.miditensor), axis=2)
+        # now the actual file build, nothing really special here
+        midifile = mido.MidiFile()
+        midifile.ticks_per_beat = ticks_per_beat
+        # now track and message building
+        for track_bytes in as_bytes:
+            track = mido.MidiTrack()
+            for message_bytes in track_bytes:
+                try:
+                    # our message format is the first three bytes are pure MIDI, then the fourth byte is time
+                    message = mido.Message.from_bytes(message_bytes[0:3])
+                    message.time = message_bytes[3]
+                    # here is a wee bit of error checking -- it is possible networks generate messages
+                    # of invalid types -- so we'll just skip those
+                    if message.type in ['note_on', 'note_off']:
+                        track.append(message)
+                # malformed messages are forgiven, particularly pad messages which are zeros
+                except ValueError:
+                    pass
+            midifile.tracks.append(track)
+        return midifile
 
 
-
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ var/scratch/readme.md
@@ -0,0 +1,1 @@
+Put test generated output here -- but otherwise ignore the folder.
